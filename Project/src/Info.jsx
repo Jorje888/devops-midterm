fb26ba175481b76046c3647bddf5a6ae50bfc293
Jorje <lursmanashviligiorgi88@gmail.com>
2025-04-20 16:25:04 +0400

    added info page and provided a route to it

---
+++ Project/src/Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function Info() {
+  return (
+    <>
+      <div>
+        <h1>What is Prime?</h1>
+        <p>Prime number is a number that is only divisible by one and itself</p>
+      </div>
+    </>
+  );
+}
+
+export default Info;
